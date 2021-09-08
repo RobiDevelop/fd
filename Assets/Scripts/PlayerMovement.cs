6285ff83d60804aa55c944d5d307e10f1a543ef6
RobiDevelop <89346615+RobiDevelop@users.noreply.github.com>
2021-09-08 19:30:00 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update() {
+        MovementInput();
+    }
+
+    void FixedUpdate() {
+        rb.velocity = movement * moveSpeed;
+    }
+
+    void MovementInput () {
+        float mx = Input.GetAxisRaw("Horizontal");
+        float my = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector2(mx, my).normalized;
+    }
+}
